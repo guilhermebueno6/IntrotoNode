b3a6ec4adf66992f111b06e2bbd6c0c9dde0a81a
unknown <guilhermebueno6@gmail.com>
2019-10-03 13:50:03 -0400

    commit

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
 const http = require('http');
+const express = require('express')
+const hbs = require('hbs');
+const path = require('path');
+const sql = require('./utils/sql');
 
-const hostname = '127.0.0.1';  // this is localhost
 const port = process.env.PORT || 3000;//this is a node convention
+const app = express();
+app.use(express.static('public'));
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname + "/views"));
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.get('/', (req, res)=>{
+  res.render('home', {homemessage: "hey there!", bio: "some generic bio info"});
+})
+
+app.get('/users', (req, res)=>{
+  //get user data when we hit this route
+
+  //try a database connection
+
+  //if the connection fails, log error(s) to the console and quit
+  sql.getConnection((err, connection)=>{
+    if (err){
+      return console.log(err.message);
+    }
+
+    let query = "SELECT * FROM tbl_card";
+
+    sql.query(query, (err, rows)=>{
+      //w're done with our DB connection, so let someone else use it 
+      connection.release();
+      //if something broke, quit and show an error essage
+      if (err){ return console.log(err.message); }
+      //show me the data
+      console.log(rows);
+
+      res.render('user', rows[0]);
+    })
+  })
+})
+
+app.listen(port, ()=>{
+  console.log(`app is running on port ${port}`);
+})
---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    uname:"root",
+    upass:"",
+    port:3306,
+    host:'localhost',
+    db:'bizcard'
+
+}
+module.exports = config;
